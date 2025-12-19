5ec805724646f790319a53d48a0e3a8076ffca71
Michasx <Michasx.malpa@gmail.com>
2025-12-19 02:18:47 +0100

    Zrobione sortowanie (to było szybkie lol)

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import csv
 #główne ustawienia początkowe głównego okna
 app = Tk()
 app.title = ("Filmy")
+
+main_color = "gray17"
+
 w = 1400
 h = 1000
 
@@ -15,7 +18,7 @@ x = (ws/2) - (w/2)
 y = (hs/2) - (h/2)
 
 app.geometry('%dx%d+%d+%d' % (w, h, x, y))
-app.configure(background="gray17")
+app.configure(background=f"{main_color}")
 
 #działania
 
@@ -33,21 +36,21 @@ file.close()
 def aktorzy(aktorzy, tytul):
     nowe_okno_2 = Tk()
     nowe_okno_2.title(f"Aktorzy - {tytul}")
-    nowe_okno_2.geometry('%dx%d+%d+%d' % (400, 300, x, y))
-    nowe_okno_2.configure(background="gray25")
+    nowe_okno_2.geometry('%dx%d+%d+%d' % (400, 300, (ws/2) - (400/2), (hs/2) - (300/2)))
+    nowe_okno_2.configure(background=f"{main_color}")
 
-    label_tytul = Label(nowe_okno_2, text=(f"Aktorzy - {tytul}"), font=('Helvetica',10,'bold'), fg="gray47", background="gray17", width=50, height=1,).pack()
-    label_aktorzy = Label(nowe_okno_2, text=(f"{aktorzy}"), font=('Helvetica',10,'bold'), fg="gray47", background="gray17", width=50, height=15,).pack()
+    label_tytul = Label(nowe_okno_2, text=(f"Aktorzy - {tytul}"), font=('Helvetica',10,'bold'), fg="gray47", background=f"{main_color}", width=50, height=1,).pack()
+    label_aktorzy = Label(nowe_okno_2, text=(f"{aktorzy}"), font=('Helvetica',10,'bold'), fg="gray47", background=f"{main_color}", width=50, height=15,).pack()
     
 
-def Wyswietl():
+def Wyswietl(Sortuj_po=None, Filtruj_po=None):
     a= 0
     b= 0
 
     nowe_okno_1 = Tk()
     nowe_okno_1.title("Wyświetlanie Filmów")
     nowe_okno_1.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    nowe_okno_1.configure(background="gray25")
+    nowe_okno_1.configure(background=f"{main_color}")
 
     frame = Frame(nowe_okno_1)
     frame.grid(row=0, column=0, sticky="nsew")
@@ -59,6 +62,9 @@ def Wyswietl():
     content_frame = Frame(canvas)
 
     content_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+    if Sortuj_po:
+        data_list.sort(key=lambda x: x[Sortuj_po])
 
 
 
@@ -72,9 +78,9 @@ def Wyswietl():
         for j in i:
             a += 1
             if j == 'Aktorzy':
-                more =Button(content_frame, text="Więcej", font=('Helvetica',9), fg="gray47", background="gray17", width=10, height=2, command=lambda i=i: aktorzy(i['Aktorzy'], i['Tytuł'])).grid(row=b, column=a, sticky="nsew")
+                more =Button(content_frame, text="Więcej", font=('Helvetica',9), fg="gray47", background=f"{main_color}", width=10, height=2, command=lambda i=i: aktorzy(i['Aktorzy'], i['Tytuł'])).grid(row=b, column=a, sticky="nsew")
             else:
-                info = Label(content_frame, text=(f"{i[j]}"), font=('Helvetica',9), fg="gray47", background="gray17", width=27, height=2,).grid(row=b, column=a, sticky="nsew")
+                info = Label(content_frame, text=(f"{i[j]}"), font=('Helvetica',9), fg="gray47", background=f"{main_color}", width=27, height=2).grid(row=b, column=a, sticky="nsew")
 
     nowe_okno_1.columnconfigure(0, weight=1)
     nowe_okno_1.rowconfigure(0, weight=1)
@@ -90,6 +96,23 @@ def Wyswietl():
 
     canvas.bind_all("<MouseWheel>", _on_mousewheel)
 
+
+def Sortuj():
+    nowe_okno_3 = Tk()
+    nowe_okno_3.title("Sortowanie Filmów")
+    nowe_okno_3.geometry('%dx%d+%d+%d' % (655, 250, (ws/2) - (400/2), (hs/2) - (300/2)))
+    nowe_okno_3.configure(background=f"{main_color}")
+
+    gora = Label(nowe_okno_3, text="Sortuj filmy po:", font=('Helvetica',15,'bold'), fg="gray47", background=f"{main_color}", width=50, height=2,).grid(row=0, column=0, columnspan=7)
+    nazwa = Button(nowe_okno_3, text="Nazwa", width=12, height=1, command=lambda : Wyswietl("Tytuł")).grid(row=1, column=0)
+    rezyser = Button(nowe_okno_3, text="Reżyser", width=12, height=1, command=lambda : Wyswietl("Reżyser")).grid(row=1, column=1)
+    aktorzy1 = Button(nowe_okno_3, text="Aktorzy", width=12, height=1, command=lambda : Wyswietl("Aktorzy")).grid(row=1, column=2)
+    rok = Button(nowe_okno_3, text="Rok", width=12, height=1, command=lambda : Wyswietl("Rok wydania")).grid(row=1, column=3)
+    gatunek = Button(nowe_okno_3, text="Gatunek", width=12, height=1, command=lambda : Wyswietl("Gatunek")).grid(row=1, column=4)
+    srednia = Button(nowe_okno_3, text="Średnia Ocena", width=12, height=1, command=lambda : Wyswietl("Średnia Ocena")).grid(row=1, column=5)
+    liczba = Button(nowe_okno_3, text="Liczba Ocen", width=12, height=1, command=lambda : Wyswietl("Liczba Ocen")).grid(row=1, column=6)
+    
+
             
 
 def Wyjscie():
@@ -103,7 +126,7 @@ tytul = Label(app, text="Biblioteka Filmów", font=('Helvetica',50), fg="gray47"
 wyswietlanie = Label(app, textvariable=data_list)
 
 show_button = Button(app, text="Wyświetl Wszystkie Filmy", width=40, height=6, command=Wyswietl)
-sort_button = Button(app, text="Sortuj po:", width=40, height=6, command=Wyswietl)
+sort_button = Button(app, text="Sortuj po:", width=40, height=6, command=Sortuj)
 filtr_button = Button(app, text="Filtruj po:", width=40, height=6, command=Wyswietl)
 add_button = Button(app, text="Dodaj nowy film", width=40, height=6, command=Wyswietl)
 delate_button = Button(app, text="Usuń film", width=40, height=6, command=Wyswietl)
